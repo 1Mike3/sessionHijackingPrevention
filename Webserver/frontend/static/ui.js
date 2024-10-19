1fab3ac977fc08bc8345ba1c3497ba272e362b15
Michal Kuc <michalkuc55@gmail.com>
2024-10-19 18:35:07 +0200

    Website basics finished, workin on modal and making website dynamic next

--- Webserver/frontend/static/ui.js
+++ Webserver/frontend/static/ui.js
@@ -1,8 +1,8 @@
 //Wait for load because css loading problem
 window.addEventListener('load', function () {
-    document.body.classList.add('loaded'); //optional
+    document.body.classList.add('loaded'); 
 
-  
+  ButtonLogin = this.document.getElementById("btn_login");
 
 
 
